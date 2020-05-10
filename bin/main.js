b00789b05601ec6cf2057df52c917db80f5526df
yepengfei <1434497449@qq.com>
2020-05-10 17:43:11 +0800

    icon file write

--- bin/main.js
+++ bin/main.js
@@ -3,6 +3,20 @@ const program = require('commander')
 const chalk = require('chalk')
 const validateProjectName = require('./utils/validateProjectName')
 const create = require('./create')
+const fs = require('fs')
+const path = require('path')
+// 临时 处理svg的文件
+var files = fs.readdirSync('E:\\Git\\biyi-cli\\bin\\svg')
+const icons = []
+for(var i=0;i<files.length;i++){
+    icons.push(files[i].slice(0, files[i].indexOf('.svg')))
+}
+
+fs.writeFile(path.resolve(__dirname, '../icons.js'), `export default ${JSON.stringify(icons)}`, function (err) {
+    if (err) {
+        console.log(chalk.red(`error: ${err}`))
+    }
+})
 
 console.log(chalk.greenBright('欢迎使用biyi-cli。'))
 
@@ -12,7 +26,7 @@ program
         // 检查项目名规范
         if (validateProjectName(name)) {
              // 启动
-             create(name, version)
+            //  create(name, version)
         }
     })
 
